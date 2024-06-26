111d82e09d7079c22dde05fbc3b6409c80176185
SeungHak <bsh93367@gmail.com>
2024-06-26 15:19:27 +0900

    add: variable.scss 추가, 필요이미지 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  sassOptions: {
+    includePaths: ['./src/styles'],
+    prependData: `@import "variable.scss";`,
+  },
 }
 
 export default nextConfig
