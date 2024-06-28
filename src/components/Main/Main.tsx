8db14cfae6ace0e65e12c63e440a3ea862b6f720
SeungHak <bsh93367@gmail.com>
2024-06-29 00:21:10 +0900

    feat: main 태그 url에 따라 색상 변경되도록 수정

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,3 +1,5 @@
+'use client'
+import { usePathname } from 'next/navigation'
 import './Main.scss'
 
 export default function Main({
@@ -5,10 +7,22 @@ export default function Main({
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const pathname = usePathname()
+  const getBgColor = () => {
+    const path = pathname.split('/')[1]
+    switch (path) {
+      case 'items':
+        return '#fff'
+      default:
+        return ''
+    }
+  }
   return (
     <>
       <div id="mainWrapper">
-        <main id="main">{children}</main>
+        <main id="main" style={{ backgroundColor: getBgColor() }}>
+          {children}
+        </main>
       </div>
     </>
   )
